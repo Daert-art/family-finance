22b5ce60155b809336b776f69139cadc72508f0b
Daert-art <131146658+Daert-art@users.noreply.github.com>
2025-07-30 21:30:48 +0300

    Исключен admin из выбора плательщиков

--- core/forms/transaction_form.py
+++ core/forms/transaction_form.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 from core.models.transaction import Transaction
 
 class TransactionForm(forms.ModelForm):
@@ -9,3 +10,10 @@ class TransactionForm(forms.ModelForm):
             'date': forms.DateInput(attrs={'type': 'date'}),
             'participants': forms.CheckboxSelectMultiple,
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Исключаем только пользователя с username='admin', но оставляем всех остальных, даже если они is_staff
+        self.fields['paid_by'].queryset = User.objects.exclude(username='admin')
+        self.fields['participants'].queryset = User.objects.exclude(username='admin')
